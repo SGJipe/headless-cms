dd8c73c3ee2f5e37fb18889d8bdb2c46fe107c0a
SGJipe <jeanpierre.han.206@gmail.com>
2022-03-23 11:50:15 +0100

    without key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://tidsaqnjlctcdbwimexw.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpZHNhcW5qbGN0Y2Rid2ltZXh3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0ODAyOTY4MiwiZXhwIjoxOTYzNjA1NjgyfQ.1eK5Wein_UoOxW2XrIemvdkHm9s17vsFPjI-35GINr8";
+const supabaseKey = "";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
